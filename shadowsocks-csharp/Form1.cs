88a2875b015c73b174d3f888fede48e97e69fe49
clowwindy <clowwindy42@gmail.com>
2013-01-14 16:17:40 +0800

    reduce dependency

--- shadowsocks-csharp/Form1.cs
+++ shadowsocks-csharp/Form1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
--- shadowsocks-csharp/Program.cs
+++ shadowsocks-csharp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace shadowsocks_csharp
