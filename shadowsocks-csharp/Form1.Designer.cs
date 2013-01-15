fe0bdbfb178b421c3340484d6809604ef8d59592
clowwindy <clowwindy42@gmail.com>
2013-01-15 20:48:40 +0800

    fix alignment of OK and Cancel button

--- shadowsocks-csharp/Form1.Designer.cs
+++ shadowsocks-csharp/Form1.Designer.cs
@@ -171,7 +171,7 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(86, 3);
+            this.button2.Location = new System.Drawing.Point(86, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
