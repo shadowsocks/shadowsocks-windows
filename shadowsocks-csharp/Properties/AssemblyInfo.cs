1c0bbfd1ab48e45a520995b8cb48ff67d10787c9
clowwindy <clowwindy42@gmail.com>
2014-11-08 16:20:46 +0800

    try adding .net 4.0 compat

--- shadowsocks-csharp/Properties/AssemblyInfo.cs
+++ shadowsocks-csharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
 // [assembly: AssemblyFileVersion("2.0.0")]
