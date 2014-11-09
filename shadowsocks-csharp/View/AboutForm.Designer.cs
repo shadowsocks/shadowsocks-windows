8af7a799b5869c8e5c1ea81314d4a50dc1b75adf
wzxjohn <wzxjohn@users.noreply.github.com>
2014-11-09 15:27:50 +0800

    add about form
    Add an about form instead of just open link.

---
+++ shadowsocks-csharp/View/AboutForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Shadowsocks.View
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.githubLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 40);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(260, 23);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Shadowsocks for Windows";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionLabel.Location = new System.Drawing.Point(12, 84);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(260, 23);
+            this.versionLabel.TabIndex = 0;
+            this.versionLabel.Text = "Version: 2.0.4.0";
+            this.versionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorLabel.Location = new System.Drawing.Point(12, 139);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(260, 23);
+            this.authorLabel.TabIndex = 0;
+            this.authorLabel.Text = "By: clowwindy";
+            this.authorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // githubLabel
+            // 
+            this.githubLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.githubLabel.Location = new System.Drawing.Point(13, 201);
+            this.githubLabel.Name = "githubLabel";
+            this.githubLabel.Size = new System.Drawing.Size(259, 23);
+            this.githubLabel.TabIndex = 1;
+            this.githubLabel.TabStop = true;
+            this.githubLabel.Text = "GitHub";
+            this.githubLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.githubLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.githubLabel_LinkClicked);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.githubLabel);
+            this.Controls.Add(this.authorLabel);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.LinkLabel githubLabel;
+    }
+}
---
+++ shadowsocks-csharp/View/AboutForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shadowsocks.View
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void githubLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/clowwindy/shadowsocks-csharp";
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
