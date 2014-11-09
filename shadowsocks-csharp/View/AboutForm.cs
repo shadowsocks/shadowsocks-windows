f5c9b10855f2c6ef868837d1e2aa723b7a33fa05
wzxjohn <wzxjohn@users.noreply.github.com>
2014-11-09 15:57:49 +0800

    add about form
    Version now change with AssemblyVersion

--- shadowsocks-csharp/View/AboutForm.cs
+++ shadowsocks-csharp/View/AboutForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -19,6 +21,14 @@ namespace Shadowsocks.View
         {
             string url = "https://github.com/clowwindy/shadowsocks-csharp";
             System.Diagnostics.Process.Start(url);
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            versionLabel.Text = "Version: " + version;
         }
     }
 }
