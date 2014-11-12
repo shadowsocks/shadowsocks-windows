c285926393576dc8210c6f4cb327c01d43f22f74
kookxiang <r18@ikk.me>
2014-11-12 14:14:03 +0800

    move code to controller/autostartup

---
+++ shadowsocks-csharp/Controller/AutoStartup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Shadowsocks.Controller {
+	class AutoStartup {
+		public static bool Set(bool enabled) {
+			try {
+				string path = Application.ExecutablePath;
+				RegistryKey runKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+				if (enabled) {
+					runKey.SetValue("Shadowsocks", path);
+				} else {
+					runKey.DeleteValue("Shadowsocks");
+				}
+				runKey.Close();
+				return true;
+			} catch (Exception) {
+				return false;
+			}
+		}
+
+		public static bool Check() {
+			try {
+				string path = Application.ExecutablePath;
+				RegistryKey runKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+				string[] runList = runKey.GetValueNames();
+				runKey.Close();
+				foreach (string item in runList) {
+					if (item.Equals("Shadowsocks"))
+						return true;
+				}
+				return false;
+			} catch (Exception) {
+				return false;
+			}
+		}
+	}
+}
