d121215c8ecbf500ae771c5959a60c36a442b1fe
clowwindy <clowwindy42@gmail.com>
2014-11-15 03:21:59 +0800

    set qrcode window background to white

--- shadowsocks-csharp/View/QRCodeForm.Designer.cs
+++ shadowsocks-csharp/View/QRCodeForm.Designer.cs
@@ -60,6 +60,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(224, 222);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
