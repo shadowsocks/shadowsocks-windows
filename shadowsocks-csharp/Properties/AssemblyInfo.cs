25b8ef71662f67f62c4c4b9d473e201a12c4f5c8
clowwindy <clowwindy42@gmail.com>
2015-01-02 00:20:19 +0800

    bump

--- shadowsocks-csharp/Properties/AssemblyInfo.cs
+++ shadowsocks-csharp/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Shadowsocks")]
-[assembly: AssemblyCopyright("Copyright © clowwindy 2014")]
+[assembly: AssemblyCopyright("Copyright © clowwindy 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
