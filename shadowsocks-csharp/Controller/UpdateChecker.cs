9a4f72465378f0754956b92545371a483c2d23a6
clowwindy <clowwindy42@gmail.com>
2015-03-06 23:21:56 +0800

    bump 2.3.1

--- shadowsocks-csharp/Controller/UpdateChecker.cs
+++ shadowsocks-csharp/Controller/UpdateChecker.cs
@@ -18,7 +18,7 @@ namespace Shadowsocks.Controller
         public string LatestVersionURL;
         public event EventHandler NewVersionFound;
 
-        public const string Version = "2.3";
+        public const string Version = "2.3.1";
 
         public void CheckUpdate(Configuration config)
         {
