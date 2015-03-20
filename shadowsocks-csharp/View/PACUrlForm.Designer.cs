4d4d02604cbf113fd8c81513146e7ad7c967aa55
Gang Zhuo <gang.zhuo@gmail.com>
2015-03-20 01:08:01 -0400

    layout by TableLayoutPanel

--- shadowsocks-csharp/View/PACUrlForm.Designer.cs
+++ shadowsocks-csharp/View/PACUrlForm.Designer.cs
@@ -32,11 +32,16 @@
             this.PACUrlLabel = new System.Windows.Forms.Label();
             this.OkButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // PACUrlTextBox
             // 
-            this.PACUrlTextBox.Location = new System.Drawing.Point(61, 12);
+            this.PACUrlTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.PACUrlTextBox.Location = new System.Drawing.Point(53, 3);
             this.PACUrlTextBox.MaxLength = 256;
             this.PACUrlTextBox.Name = "PACUrlTextBox";
             this.PACUrlTextBox.Size = new System.Drawing.Size(245, 20);
@@ -45,16 +50,19 @@
             // 
             // PACUrlLabel
             // 
+            this.PACUrlLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.PACUrlLabel.AutoSize = true;
-            this.PACUrlLabel.Location = new System.Drawing.Point(6, 15);
+            this.PACUrlLabel.Location = new System.Drawing.Point(3, 6);
+            this.PACUrlLabel.Margin = new System.Windows.Forms.Padding(3);
             this.PACUrlLabel.Name = "PACUrlLabel";
             this.PACUrlLabel.Size = new System.Drawing.Size(44, 13);
             this.PACUrlLabel.TabIndex = 3;
             this.PACUrlLabel.Text = "PAC Url";
+            this.PACUrlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // OkButton
             // 
-            this.OkButton.Location = new System.Drawing.Point(150, 50);
+            this.OkButton.Location = new System.Drawing.Point(3, 3);
             this.OkButton.Name = "OkButton";
             this.OkButton.Size = new System.Drawing.Size(75, 23);
             this.OkButton.TabIndex = 5;
@@ -64,7 +72,7 @@
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(231, 50);
+            this.CancelButton.Location = new System.Drawing.Point(84, 3);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(75, 23);
             this.CancelButton.TabIndex = 6;
@@ -72,24 +80,60 @@
             this.CancelButton.UseVisualStyleBackColor = true;
             this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.PACUrlLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PACUrlTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 9);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(301, 61);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.OkButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CancelButton, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(136, 29);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(162, 29);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
             // PACUrlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(327, 88);
-            this.Controls.Add(this.CancelButton);
-            this.Controls.Add(this.OkButton);
-            this.Controls.Add(this.PACUrlTextBox);
-            this.Controls.Add(this.PACUrlLabel);
+            this.ClientSize = new System.Drawing.Size(351, 101);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PACUrlForm";
+            this.Padding = new System.Windows.Forms.Padding(3);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Update Online PAC URL";
             this.Load += new System.EventHandler(this.PACUrlForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +145,7 @@
         private System.Windows.Forms.Label PACUrlLabel;
         private System.Windows.Forms.Button OkButton;
         private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
