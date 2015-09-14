7a5451976cbc70df0b357327f8def35db5815946
kimw <1@kimwong.me>
2015-09-14 12:58:15 +0800

    fixed: remove useless exception throw out.

--- shadowsocks-csharp/Model/LogViewerConfig.cs
+++ shadowsocks-csharp/Model/LogViewerConfig.cs
@@ -60,7 +60,6 @@ namespace Shadowsocks.Model
             catch (Exception)
             {
                 return ColorTranslator.FromHtml("white");
-                throw;
             }
         }
 
