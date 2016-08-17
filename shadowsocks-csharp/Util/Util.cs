ddb73744321b586f5666f07853f861a32b5751c7
Syrone Wong <wong.syrone@gmail.com>
2016-08-17 10:31:21 +0800

    Refine Windows registry again(#512)
    Used a method added in .NET 4, user may not have
    this version installed, reverting to an older
    method seems to be a better solution.
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- shadowsocks-csharp/Util/Util.cs
+++ shadowsocks-csharp/Util/Util.cs
@@ -151,9 +151,9 @@ namespace Shadowsocks.Util
             // we are building x86 binary for both x86 and x64, which will
             // cause problem when opening registry key
             // detect operating system instead of CPU
-            RegistryKey userKey = RegistryKey.OpenBaseKey( RegistryHive.CurrentUser,
-                Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32 );
-            userKey = userKey.OpenSubKey( name, writable );
+            RegistryKey userKey = RegistryKey.OpenRemoteBaseKey( RegistryHive.CurrentUser, "",
+                Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32 )
+                .OpenSubKey( name, writable );
             return userKey;
         }
 
