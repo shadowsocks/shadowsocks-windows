de4a2f862ba86a32e25eadcc8df93aafd129fe3b
noisyfox <timemanager.rick@gmail.com>
2016-12-09 02:47:12 +1100

    Update global proxy bypass list

--- shadowsocks-csharp/Util/SystemProxy/Sysproxy.cs
+++ shadowsocks-csharp/Util/SystemProxy/Sysproxy.cs
@@ -44,11 +44,11 @@ namespace Shadowsocks.Util.SystemProxy
             {
                 if (global)
                 {
-                    arguments = "global " + proxyServer;
+                    arguments = $"global {proxyServer} <local>localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;172.32.*;192.168.*";
                 }
                 else
                 {
-                    arguments = "pac " + pacURL;
+                    arguments = $"pac {pacURL}";
                 }
             }
             else
