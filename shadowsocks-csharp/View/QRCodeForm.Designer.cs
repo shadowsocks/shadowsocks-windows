b972cb250bd66c7be7feea2ff544d18a5b9d08d4
noisyfox <timemanager.rick@gmail.com>
2016-12-13 02:31:01 +1100

    Don't zoom QRCode

--- shadowsocks-csharp/View/QRCodeForm.Designer.cs
+++ shadowsocks-csharp/View/QRCodeForm.Designer.cs
@@ -40,7 +40,7 @@
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(210, 210);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
