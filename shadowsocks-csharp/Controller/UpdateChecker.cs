bb5c6cbd2f0138b6553aecbc33bcda5177515690
破娃酱 <mmgac001@gmail.com>
2017-01-03 20:56:33 +0800

    bump

--- shadowsocks-csharp/Controller/UpdateChecker.cs
+++ shadowsocks-csharp/Controller/UpdateChecker.cs
@@ -21,7 +21,7 @@ namespace Shadowsocks.Controller
 
         public const string Name = "ShadowsocksR";
         public const string Copyright = "Copyright © BreakWall 2016. Fork from Shadowsocks by clowwindy";
-        public const string Version = "4.0.3";
+        public const string Version = "4.0.4";
 #if !_DOTNET_4_0
         public const string NetVer = "2.0";
 #elif !_CONSOLE
