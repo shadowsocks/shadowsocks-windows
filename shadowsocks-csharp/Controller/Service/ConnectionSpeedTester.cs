c2c4c099392ece94575047d58d6fd1fbcd6eed07
Mygod <contact-git@mygod.be>
2018-06-19 15:10:44 +0800

    Refine for 2018

--- shadowsocks-csharp/Controller/Service/ConnectionSpeedTester.cs
+++ shadowsocks-csharp/Controller/Service/ConnectionSpeedTester.cs
@@ -37,27 +37,15 @@ namespace Shadowsocks.Controller.Service
                         Writer.WriteLine($"Testing {server.FriendlyName()}...");
                         Controller.SelectServerIndex(i);
                         var tester = new DownloadThread(proxy);
-                        var timeout = false;
                         var stopwatch = Stopwatch.StartNew();
                         tester.Thread.Start();
-                        if (tester.Thread.Join(4000)) stopwatch.Stop();
-                        else
-                        {
-                            tester.Stopped = true;
-                            stopwatch.Stop();
-                            timeout = true;
-                            if (Stopped) break;
-                            Writer.WriteLine("Test timed out.");
-                        }
+                        if (!tester.Thread.Join(5000)) tester.Stopped = true;
+                        stopwatch.Stop();
+                        if (Stopped) break;
                         var secs = stopwatch.Elapsed.TotalSeconds;
                         var speed = tester.Size / secs;
-                        if (Stopped) break;
                         Writer.WriteLine($"Downloaded {GetSize(tester.Size)} in {secs}s, average {GetSize(speed)}/s.");
-                        var result = tester.Size <= 0 ? "dead"
-                            : (timeout ? "*" : string.Empty) + GetSizeShort((long)speed);
-                        server.remarks = string.IsNullOrWhiteSpace(server.remarks) ||
-                            FullMatcher.Match(server.remarks).Success ? result
-                                : EndingRemoval.Replace(server.remarks, string.Empty) + " (" + result + ')';
+                        server.remarks = tester.Size <= 0 ? "dead" : GetSizeShort((long)speed);
                         if (speed < maxSpeed) continue;
                         maxSpeed = speed;
                         max = i;
@@ -85,7 +73,7 @@ namespace Shadowsocks.Controller.Service
             public DownloadThread(IWebProxy proxy)
             {
                 request = (HttpWebRequest)
-                    WebRequest.Create("https://dl-ssl.google.com/googletalk/googletalk-setup.exe");
+                    WebRequest.Create("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BF8B7F175-9454-645A-CF7A-CBB3458688B8%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1/chrome/install/ChromeStandaloneSetup64.exe");
                 request.Proxy = proxy;
                 request.ReadWriteTimeout = request.Timeout = 4000;
                 Thread = new Thread(Process);
@@ -138,10 +126,7 @@ namespace Shadowsocks.Controller.Service
             e.Cancel = true;
             currentConfig.Stopped = true;
         }
-
-        private static readonly Regex FullMatcher = new Regex(@"^(dead|\*?\d+( Bytes|\w))$", RegexOptions.Compiled),
-            EndingRemoval = new Regex(@" \((dead|\*?\d+( Bytes|\w))\)$", RegexOptions.Compiled);
-
+        
         private static readonly string[]
             Units = { null, "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB", "BB", "NB", "DB", "CB" };
 
@@ -170,7 +155,7 @@ namespace Shadowsocks.Controller.Service
             return n.ToString("N") + ' ' + (i == 0 ? bytes : Units[i] + " (" + size.ToString("N") + ' ' + bytes + ')');
         }
 
-        public static string GetSizeShort(long size, string bytes = "Bytes")
+        public static string GetSizeShort(double size, string bytes = "Bytes")
         {
             var n = size;
             byte i = 0;
@@ -179,7 +164,7 @@ namespace Shadowsocks.Controller.Service
                 n /= 1024;
                 i++;
             }
-            return n + (i == 0 ? ' ' + bytes : Units[i].Substring(0, 1));
+            return n.ToString("N") + ' ' + (i == 0 ? bytes : Units[i]);
         }
     }
 }
