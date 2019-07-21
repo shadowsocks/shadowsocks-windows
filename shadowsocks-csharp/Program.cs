418243bf6f87a95fbc480c290770eb09c1e002f9
谭九鼎 <109224573@qq.com>
2019-07-21 22:20:23 +0800

    Update Program.cs

--- shadowsocks-csharp/Program.cs
+++ shadowsocks-csharp/Program.cs
@@ -26,7 +26,7 @@ namespace Shadowsocks
         {
             // .NET Framework 4.7.2 on Win7 compatibility
             System.Net.ServicePointManager.SecurityProtocol |= 
-                SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12
+                SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
             // store args for further use
             Args = args;
