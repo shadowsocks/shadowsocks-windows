afa370b1201e1328b4f37dfed5f2fe08fd1d7aa9
celeron533 <zhuyilun8@hotmail.com>
2019-12-21 23:10:54 +0800

    Update CHANGES and bump version

--- shadowsocks-csharp/Controller/Service/UpdateChecker.cs
+++ shadowsocks-csharp/Controller/Service/UpdateChecker.cs
@@ -24,7 +24,7 @@ namespace Shadowsocks.Controller
         public string LatestVersionLocalName;
         public event EventHandler CheckUpdateCompleted;
 
-        public const string Version = "4.1.8.0";
+        public const string Version = "4.1.9.0";
 
         private class CheckUpdateTimer : System.Timers.Timer
         {
