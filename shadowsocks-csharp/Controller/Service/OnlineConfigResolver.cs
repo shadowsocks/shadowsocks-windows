608675f96e12cb3de13ab451f5b997783db1a057
database64128 <free122448@hotmail.com>
2020-10-12 00:18:58 +0800

    ⚙ Allow configuration of User-Agent
    - For OnlineConfigResolver and GeositeUpdater

--- shadowsocks-csharp/Controller/Service/OnlineConfigResolver.cs
+++ shadowsocks-csharp/Controller/Service/OnlineConfigResolver.cs
@@ -11,7 +11,7 @@ namespace Shadowsocks.Controller.Service
 {
     public class OnlineConfigResolver
     {
-        public static async Task<List<Server>> GetOnline(string url, IWebProxy proxy = null)
+        public static async Task<List<Server>> GetOnline(string url, string userAgentString, IWebProxy proxy = null)
         {
             var httpClientHandler = new HttpClientHandler()
             {
@@ -21,9 +21,8 @@ namespace Shadowsocks.Controller.Service
             {
                 Timeout = TimeSpan.FromSeconds(15)
             };
-
-            string userAgent = "ShadowsocksWindows/" + UpdateChecker.Version;
-            httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+            if (!string.IsNullOrWhiteSpace(userAgentString))
+                httpClient.DefaultRequestHeaders.Add("User-Agent", userAgentString);
 
             string server_json = await httpClient.GetStringAsync(url);
 
